2c0bc51d0ddc55b7e6d55c61c962f3d1dcb0cb98
Gennadiy Khudyakov <lHooFool@gmail.com>
2016-12-20 16:16:12 +0700

    Making messenger

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -1,11 +1,12 @@
 class MessengerController
   def webhook
     Rails.logger.debug params.inspect
-    #logic here
-    if params['hub.mode'] == 'subscribe' && params['hub.verify_token'] == Messenger.config.verify_token
-      render text: params['hub.challenge'], status: 200
-    else
-      render nothing: true, status: 200
-    end
+    # #logic here
+    # if params['hub.mode'] == 'subscribe' && params['hub.verify_token'] == Messenger.config.verify_token
+    #   render text: params['hub.challenge'], status: 200
+    # else
+    # end
+    render nothing: true, status: 200
+
   end
 end
