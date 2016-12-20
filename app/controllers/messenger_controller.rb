abfeb509c21b6f489b5ab83a0b6a6d41f82149b9
Gennadiy Khudyakov <lHooFool@gmail.com>
2016-12-20 15:52:30 +0700

    Webhook setup

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -1,6 +1,10 @@
 class MessengerController
   def webhook
     #logic here
-    render nothing: true, status: 200
+    if params['hub.mode'] == 'subscribe' && params['hub.verify_token'] == Messenger.config.verify_token
+      render text: params['hub.challenge'], status: 200
+    else
+      render nothing: true, status: 200
+    end
   end
 end
